fd14fb6ee2810e148ebe334bd75f9d7d2ba01cdb
sunny <chpvsaikumar@gmail.com>
2023-02-12 11:45:16 -0600

    initial

---
+++ src/main/java/com/example/rga/RgaApplication.java
@@ -0,0 +1,13 @@
+package com.example.rga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RgaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RgaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/rga/RgaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RgaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
